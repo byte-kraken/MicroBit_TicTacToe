8c2f0fa5dc6eef2ea12479fc44e68faf57701ff5
byte-kraken <fehstrash@gmail.com>
2024-04-05 18:45:45 +0200

    Added Win Logic Draft

--- main.ts
+++ main.ts
@@ -1,6 +1,3 @@
-function checkWin () {
-	
-}
 function movePointer () {
     if (my_pos.get(LedSpriteProperty.X) >= 2) {
         my_pos.set(LedSpriteProperty.X, 0)
@@ -14,6 +11,17 @@ function movePointer () {
         my_pos.change(LedSpriteProperty.X, 1)
     }
 }
+function checkCol (col: number) {
+    return cache[calcIndex(1, col)].get(LedSpriteProperty.Brightness) == cache[calcIndex(2, col)].get(LedSpriteProperty.Brightness) && cache[calcIndex(1, col)].get(LedSpriteProperty.Brightness) == cache[calcIndex(3, col)].get(LedSpriteProperty.Brightness)
+}
+function checkDraw () {
+    for (let index = 0; index <= 3; index++) {
+        for (let index = 0; index <= 3; index++) {
+        	
+        }
+    }
+    return 0
+}
 function checkValid (x: number, y: number) {
     if (cache[calcIndex(x, y)] != null) {
         return false
@@ -23,13 +31,28 @@ function checkValid (x: number, y: number) {
 input.onButtonPressed(Button.A, function () {
     movePointer()
 })
+function checkDia (dia: number) {
+	
+}
+function checkEnd (x: number, y: number) {
+    if (checkRow(x) || (checkCol(y) || false)) {
+        if (your_turn) {
+            music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Entertainer), music.PlaybackMode.InBackground)
+        } else {
+            music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Dadadadum), music.PlaybackMode.InBackground)
+        }
+    }
+    if (checkDraw()) {
+        music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Funk), music.PlaybackMode.InBackground)
+    }
+}
 input.onButtonPressed(Button.B, function () {
     if (your_turn && checkValid(my_pos.get(LedSpriteProperty.X), my_pos.get(LedSpriteProperty.Y))) {
         cache[calcIndex(my_pos.get(LedSpriteProperty.X), my_pos.get(LedSpriteProperty.Y))] = game.createSprite(my_pos.get(LedSpriteProperty.X), my_pos.get(LedSpriteProperty.Y))
         cache[calcIndex(my_pos.get(LedSpriteProperty.X), my_pos.get(LedSpriteProperty.Y))].set(LedSpriteProperty.Brightness, 180)
         radio.sendValue("x", my_pos.get(LedSpriteProperty.X))
         radio.sendValue("y", my_pos.get(LedSpriteProperty.Y))
-        checkWin()
+        checkEnd(my_pos.get(LedSpriteProperty.X), my_pos.get(LedSpriteProperty.Y))
         your_turn = false
         music._playDefaultBackground(music.builtInPlayableMelody(Melodies.BaDing), music.PlaybackMode.InBackground)
     } else {
@@ -48,9 +71,12 @@ radio.onReceivedValue(function (name, value) {
         cache[calcIndex(radio_pos.get(LedSpriteProperty.X), radio_pos.get(LedSpriteProperty.Y))] = game.createSprite(radio_pos.get(LedSpriteProperty.X), radio_pos.get(LedSpriteProperty.Y))
         cache[calcIndex(radio_pos.get(LedSpriteProperty.X), radio_pos.get(LedSpriteProperty.Y))].set(LedSpriteProperty.Brightness, 20)
     }
-    checkWin()
+    checkEnd(radio_pos.get(LedSpriteProperty.X), radio_pos.get(LedSpriteProperty.Y))
     your_turn = true
 })
+function checkRow (row: number) {
+    return cache[calcIndex(row, 1)].get(LedSpriteProperty.Brightness) == cache[calcIndex(row, 2)].get(LedSpriteProperty.Brightness) && cache[calcIndex(row, 1)].get(LedSpriteProperty.Brightness) == cache[calcIndex(row, 3)].get(LedSpriteProperty.Brightness)
+}
 let cache: game.LedSprite[] = []
 let your_turn = false
 let radio_pos: game.LedSprite = null
